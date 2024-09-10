fc7469b01b05a303cd3767082f7004bb87000117
yaoleksa <mryaremchyk@gmail.com>
2024-09-10 19:01:54 +0300

    ?

--- g.py
+++ g.py
@@ -1,65 +1,49 @@
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+import google.auth
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = "14nKyYCD0SOunCobchyeXjuaxAWyUpOjf_AMu6_swcrs"
-SAMPLE_RANGE_NAME = "Data!A2:E"
-
-
-def main():
-  """Shows basic usage of the Sheets API.
-  Prints values from a sample spreadsheet.
+def update_values(spreadsheet_id, range_name, value_input_option, _values):
   """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
-
+  Creates the batch_update the user has access to.
+  Load pre-authorized user credentials from the environment.
+  TODO(developer) - See https://developers.google.com/identity
+  for guides on implementing OAuth2 for the application.
+  """
+  creds, _ = google.auth.default()
+  # pylint: disable=maybe-no-member
   try:
     service = build("sheets", "v4", credentials=creds)
-
-    # Call the Sheets API
-    sheet = service.spreadsheets()
+    values = [
+        [
+            # Cell values ...
+        ],
+        # Additional rows ...
+    ]
+    body = {"values": _values}
     result = (
-        sheet.values()
-        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+        service.spreadsheets()
+        .values()
+        .update(
+            spreadsheetId=spreadsheet_id,
+            range=range_name,
+            valueInputOption=value_input_option,
+            body=body,
+        )
         .execute()
     )
-    values = result.get("values", [])
-
-    if not values:
-      print("No data found.")
-      return
-
-    print("Name, Major:")
-    for row in values:
-      # Print columns A and E, which correspond to indices 0 and 4.
-      print(f"{row[0]}, {row[4]}")
-  except HttpError as err:
-    print(err)
+    print(f"{result.get('updatedCells')} cells updated.")
+    return result
+  except HttpError as error:
+    print(f"An error occurred: {error}")
+    return error
 
 
 if __name__ == "__main__":
-  main()
+  # Pass: spreadsheet_id,  range_name, value_input_option and  _values
+  update_values(
+      "1CM29gwKIzeXsAppeNwrc8lbYaVMmUclprLuLYuHog4k",
+      "A1:C2",
+      "USER_ENTERED",
+      [["A", "B"], ["C", "D"]],
+  )
