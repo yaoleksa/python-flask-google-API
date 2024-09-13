4cc34924d29f960df00577408aae23540ca2608b
yaoleksa <mryaremchyk@gmail.com>
2024-09-13 15:22:21 +0300

    setup authentefication

--- currencyAPI.py
+++ currencyAPI.py
@@ -1,4 +1,5 @@
 # A very simple API call to Google Sheet
+import os
 import pygsheets
 
 import pandas as pd
@@ -8,6 +9,13 @@ from flask import Flask, request
 from collections import defaultdict
 from datetime import datetime, date
 
+# init Google Sheets client
+client = (
+    pygsheets
+    .authorize(service_file='./currencyapi-435306-73cfd4ea9d84.json')
+    .open_by_key('1kDmxlJpwOgVEO3h9AStLyVYC3cYtzy30MqV0X2WqnL8')
+)
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -15,6 +23,9 @@ def hello_world():
 
     # define POST HTTP HANDLER
     if request.method == 'POST':
+        # check API key
+        if request.headers.get('X-API-Key') != os.getenv('X-API-Key'):
+            return 'Missing or invalid API key'
         today = date.today()
         update_from = datetime.strftime(today, '%Y-%m-%d')
         update_to = update_from
@@ -32,6 +43,13 @@ def hello_world():
         for item in exchange_rates:
             to_insert['exchangedate'].append(item['exchangedate'])
             to_insert['rate'].append(str(item['rate']).replace('.', ','))
+        # Create dataframe to write to sheet
+        df = pd.DataFrame(data={
+            'exchangedate': to_insert['exchangedate'],
+            'rate': to_insert['rate']
+        })
+        # write to sheet
+        client[2].set_dataframe(df, (1, 1))
         return to_insert
 
     return 'oki'
