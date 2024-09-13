4d2ec03bf62d63740586741878cb10a626a25532
yaoleksa <mryaremchyk@gmail.com>
2024-09-13 14:11:53 +0300

    improve my solution

--- currencyAPI.py
+++ currencyAPI.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 from requests import get
 from flask import Flask, request
+from collections import defaultdict
 from datetime import datetime, date
 
 app = Flask(__name__)
@@ -24,7 +25,13 @@ def hello_world():
             update_to = data['update_to']
         update_from = str(update_from).replace('-', '')
         update_to = str(update_to).replace('-', '')
-        print(f"https://bank.gov.ua/NBU_Exchange/exchange_site?start={update_from}&end={update_to}&valcode=usd&sort=exchangedate&order=desc&json")
-        return get(f'https://bank.gov.ua/NBU_Exchange/exchange_site?start={update_from}&end={update_to}&valcode=usd&sort=exchangedate&order=desc&json').json()
+        # get currency rate for period
+        exchange_rates = get(f'https://bank.gov.ua/NBU_Exchange/exchange_site?start={update_from}&end={update_to}&valcode=usd&sort=exchangedate&order=desc&json').json()
+        # data to insert into sheet
+        to_insert = defaultdict(list)
+        for item in exchange_rates:
+            to_insert['exchangedate'].append(item['exchangedate'])
+            to_insert['rate'].append(str(item['rate']).replace('.', ','))
+        return to_insert
 
     return 'oki'
