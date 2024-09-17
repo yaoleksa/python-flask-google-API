a0594a1357c9367b582c63f8783861af2c96eaa6
yaoleksa <mryaremchyk@gmail.com>
2024-09-17 11:58:18 +0300

    a little improving

--- app.py
+++ app.py
@@ -55,6 +55,6 @@ def hello_world():
     return '''
             This is a simple implementation of Google API with external NBU currency exchange API. The server is hosted on this https://yaoleksa.pythonanywhere.com/ web address.
             API supports the POST HTTP method. To get currency exchange rates for the period just make a POST request to 
-            https://yaoleksa.pythonanywhere.com/?update_from=`YYYYMMDD`&updateto=`YYYYMMDD` with this API key `ffda1910-cab3-44fc-96e7-86074b8b865e` 
+            https://yaoleksa.pythonanywhere.com/?update_from=`YYYYMMDD`&update_to=`YYYYMMDD` with this API key `ffda1910-cab3-44fc-96e7-86074b8b865e` 
             added to request header.
         '''
